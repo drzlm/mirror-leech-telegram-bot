1a91efac2961408258bdc6bd87e7a79490c36eb4
drzlm <86509636+drzlm@users.noreply.github.com>
2023-07-07 22:26:47 +0630

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
-from os import path as ospath, environ, remove
+from os import path as ospath, environ
 from subprocess import run as srun
 from requests import get as rget
 from dotenv import load_dotenv, dotenv_values
@@ -9,12 +9,9 @@ if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
         f.truncate(0)
 
-if ospath.exists('rlog.txt'):
-    remove('rlog.txt')
-
 basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            handlers=[FileHandler('log.txt'), StreamHandler()],
-            level=INFO)
+                    handlers=[FileHandler('log.txt'), StreamHandler()],
+                    level=INFO)
 
 CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
 try:
@@ -31,7 +28,7 @@ try:
         log_error(f"CONFIG_FILE_URL: {e}")
 except:
     pass
-    
+
 load_dotenv('config.env', override=True)
 
 try:
@@ -60,14 +57,14 @@ if DATABASE_URL is not None:
     if old_config is not None:
         del old_config['_id']
     if (old_config is not None and old_config == dict(dotenv_values('config.env')) or old_config is None) \
-            and config_dict is not None:
+           and config_dict is not None:
         environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
         environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
     conn.close()
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = "https://github.com/drzlm/mirror-leech-telegram-bot"
+   UPSTREAM_REPO = "https://github.com/drzlm/mirror-leech-telegram-bot"
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
@@ -89,5 +86,4 @@ if UPSTREAM_REPO is not None:
     if update.returncode == 0:
         log_info('Successfully updated with latest commit from UPSTREAM_REPO')
     else:
-        log_error(
-            'Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
